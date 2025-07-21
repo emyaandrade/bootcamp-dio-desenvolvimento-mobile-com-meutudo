5032a55baef39b1fca8809d9a111a90b0787c5b9
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-07-21 11:41:02 -0300

    aula de introdução: pré-requisitos

---
+++ 01-pre-requesitos/01-aula-de-introducao.js
@@ -0,0 +1,14 @@
+//Comece por lógica de programação (variável, for, if, laço de repetição, IDE)
+//Depois tenha pelo menos alguma noção de JavaScript
+
+//O node.js não é uma linguagem de programação, ele é uma ferramenta e nessa ferramenta a gente precisa utilizar JavaScript em boa parte do tempo para escrever o código.
+
+//Checklist do que preciso saber em JavaScript para começar o node.js:
+//Sintaxe básica: variáveis e constantes, tipos de dados, operadores matemáticos (como somar, comparar, etc)
+//Estruturas de Controle Condicional: if, else, while
+//Estruturas de repetição: for, while, do while
+//Funções: declaração de funções, parâmetros e argumentos, retorno de funções, funções anônimas, arrow functions, escopo de funções
+//Arrays: declaracção de array, métodos de array(map, filter, reduce, foreach), literação com for...of, spread operator e rest parameters
+//Objetos e prototypes: criando objetos, propriedades e métodos, prototypes e herança
+//assincronismo: callbacks, promises, async/await
+//tratamento de erros: erros nativos, try-catch, throw
